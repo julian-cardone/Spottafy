661cd3100973904b72cfa28feebbabe9f863a19a
Julian Cardone <juliancardone1@gmail.com>
2022-12-04 13:23:58 -0500

    backend user auth built out

--- Spottafy/config/routes.rb
+++ Spottafy/config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: :create
+    resource :session, only: [:show, :create, :destroy]
+  end
+
+  get '*path', to: "static_pages#frontend_index"
+
 end
