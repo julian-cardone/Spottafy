4bbb657e108b0d1b36f4cd91616248a9eb43267f
Julian Cardone <juliancardone1@gmail.com>
2022-12-05 11:40:54 -0500

    backend auth is tested and working

--- Spottafy/app/models/user.rb
+++ Spottafy/app/models/user.rb
@@ -23,29 +23,33 @@ class User < ApplicationRecord
     before_validation :ensure_session_token
 
     def self.find_by_credentials(credential, password)
-        field = credential == URI::MailTo::EMAIL_REGEXP ? :email : :username
-        user = User.find_by(field: credential)
-        user&.authenticate(password)
+        field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+        user = User.find_by(field => credential)
+        if user&.authenticate(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
     end
 
     def reset_session_token!
-        session_token = generate_unique_session_token
-        self.update!
-        self.session_token
+        self.session_token = generate_unique_session_token
+        save!
+        session_token
     end
 
     private
 
     def generate_unique_session_token
-        while true do
+        while true
             token = SecureRandom.urlsafe_base64
-            User.exists?(session_token: token) ? next : break 
+            return token unless User.exists?(session_token: token)
             #seems inefficient, what if there are a million users? is there a better way to do this?
         end
-    end
-
-    def ensure_session_token
-        self.session_token ||= reset_session_token!
     end
 
 end
