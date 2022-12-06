5322b0ad325f1435f5b279d337a00a7947e903bf
Julian Cardone <juliancardone1@gmail.com>
2022-12-06 12:01:56 -0500

    working

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,9 +37,8 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        self.session_token = generate_unique_session_token
-        save!
-        session_token
+        self.update!(session_token: generate_unique_session_token)
+        self.session_token
     end
 
     private
