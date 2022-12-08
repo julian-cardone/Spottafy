061bf7443182224af51bd88d6a35f19d1638a3d1
Julian Cardone <juliancardone1@gmail.com>
2022-12-08 11:47:18 -0500

    finished the stylingn for login page

--- frontend/src/components/SessionForm/LoginForm.js
+++ frontend/src/components/SessionForm/LoginForm.js
@@ -21,7 +21,7 @@ function LoginForm({ onSuccess }) {
             Email address or username
           </label>
         </div>
-          <input label="username"value={credential}onChange={onCredentialChange}placeholder="Email Address or username"required></input>
+          <input label="username"value={credential}onChange={onCredentialChange}placeholder="Email address or username"required></input>
       </div>
  
       <div className="password-login-container">
@@ -30,10 +30,19 @@ function LoginForm({ onSuccess }) {
             Password
           </label>
         </div>
-          <input label="password"value={password}onChange={onPasswordChange}placeholder="Password"required></input>
+          <input label="password"type="password"value={password}onChange={onPasswordChange}placeholder="Password"required></input>
       </div>
 
     <div className="login-button-container">
+
+    <div className="demo-login-button-li-page">
+      <button type="submit" className="demo-login-button-li-page">
+        <div className="inner-demo-login-li-page">
+          <p className="demo-log-in-text-li-page">DEMO USER</p>
+        </div>
+      </button>
+      </div>
+
       <div className="login-button-li-page">
       <button type="submit" className="login-button-li-page">
         <div className="inner-login-li-page">
@@ -41,6 +50,7 @@ function LoginForm({ onSuccess }) {
         </div>
       </button>
       </div>
+
     </div>
     </form>
   );
