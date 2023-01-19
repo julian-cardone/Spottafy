bcfe76f37e84070e947ee89747689f41ee1b7bcf
Julian Cardone <juliancardone1@gmail.com>
2023-01-19 18:42:26 -0500

    added the white circle in the seek bar

--- frontend/src/components/AudioBar/SeekBar.js
+++ frontend/src/components/AudioBar/SeekBar.js
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
 const SeekBar = ({ currentSong, audioElement }) => {
-
   // audioElement.current.currentTime = 0;
 
   const clickRef = useRef();
@@ -9,10 +8,10 @@ const SeekBar = ({ currentSong, audioElement }) => {
   const checkWidth = (e) => {
     let width = clickRef.current.clientWidth;
     const offset = e.nativeEvent.offsetX;
-    
-    const divprogress = offset / width * 100;
-    audioElement.current.currentTime = divprogress / 100 * currentSong.length;
-  }
+
+    const divprogress = (offset / width) * 100;
+    audioElement.current.currentTime = (divprogress / 100) * currentSong.length;
+  };
 
   // const [seconds, setSeconds] = useState(0);
   // const [minutes, setMinutes] = useState(0);
@@ -22,20 +21,34 @@ const SeekBar = ({ currentSong, audioElement }) => {
   // },[])
 
   return (
-    <div className='seeking-bar'>
-              <div className='first-number'>{`${Math.floor(currentSong.ct/60)||"0"}:${Math.floor(currentSong.ct/10%60%6)||"0"}${Math.floor(currentSong.ct%60%10)||"0"}`}</div>
-              <div className='seeking-bar-wrap'onClick={(e) => checkWidth(e)} ref={clickRef}>
-                <div className='seeking-bar-inner'>
-                  <div className='seeking-bar-inner-inner'>
-                    <div className='progress-bar'>
-                      <div className='progress-bar-inner'style={{width: `${currentSong.progress+"%"}`}}></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='second-number'>{`${Math.floor(currentSong.length / 60)||"0"}:${Math.floor(currentSong.length%60)||"00"}`}</div>
+    <div className="seeking-bar">
+      <div className="first-number">{`${
+        Math.floor(currentSong.ct / 60) || "0"
+      }:${Math.floor(((currentSong.ct / 10) % 60) % 6) || "0"}${
+        Math.floor((currentSong.ct % 60) % 10) || "0"
+      }`}</div>
+      <div
+        className="seeking-bar-wrap"
+        onClick={(e) => checkWidth(e)}
+        ref={clickRef}
+      >
+        <div className="seeking-bar-inner">
+          <div className="seeking-bar-inner-inner">
+            <div className="progress-bar">
+              <div
+                className="progress-bar-inner"
+                style={{ width: `${currentSong.progress + "%"}` }}
+              ></div>
             </div>
-  )
-}
+          </div>
+        </div>
+          <div className="circle-on-seek"style={{left: `${currentSong.progress - 1 + "%"}`}}></div>
+      </div>
+      <div className="second-number">{`${
+        Math.floor(currentSong.length / 60) || "0"
+      }:${Math.floor(currentSong.length % 60) || "00"}`}</div>
+    </div>
+  );
+};
 
 export default SeekBar;
