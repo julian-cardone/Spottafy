cb9fc53e54d18e3ecefc591438e61b3faf7755dd
Julian Cardone <juliancardone1@gmail.com>
2023-03-06 12:45:11 -0500

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,8 @@ set -o errexit
 ls
 npm run build
 bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 rails db:migrate
 rails db:seed #if needed
